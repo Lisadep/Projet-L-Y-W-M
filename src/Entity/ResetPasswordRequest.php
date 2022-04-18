ae24ba84b25b2c133b751b84265bb37fb4da1529
unknown <depierrepont.lisa@gmail.com>
2022-04-18 17:50:43 +0200

    Mise en place de la réinitialisation de mot de passe

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -6,7 +6,6 @@ use App\Repository\ResetPasswordRequestRepository;
 use Doctrine\ORM\Mapping as ORM;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
-
 #[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
 class ResetPasswordRequest implements ResetPasswordRequestInterface
 {
--- src/Repository/ResetPasswordRequestRepository.php
+++ src/Repository/ResetPasswordRequestRepository.php
@@ -2,14 +2,15 @@
 
 namespace App\Repository;
 
-use App\Entity\ResetPasswordRequest;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use App\Entity\ResetPasswordRequest;
+use Doctrine\ORM\OptimisticLockException;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
-use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
+
 
 /**
  * @method ResetPasswordRequest|null find($id, $lockMode = null, $lockVersion = null)
